d5b932c139098106439fc19e9b93984c796e28df
shreesainath1983 <153953721+shreesainath1983@users.noreply.github.com>
2024-12-01 13:58:23 +0530

    Update config.js

--- Dashboard/js/custom/config.js
+++ Dashboard/js/custom/config.js
@@ -10,4 +10,10 @@ const userCredentials=[{
     id:1,username:'admin',pass:'admin2023',name:'Admin'
 },{
     id:6,username:'other',pass:'202324',name:'Other user'
+},{
+    id:1,username:'shubh123',pass:'202324',name:'Shubh Barot'
+},{
+    id:1,username:'shubham123',pass:'202324',name:'Shubham Dubey'
+},{
+    id:1,username:'neeraj123',pass:'202324',name:'Neeraj Sikhwal'
 }]
